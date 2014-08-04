0c7839f3c3ca5e2e20c93f099b3849bb6db08a84
Joost Bremmer <toost.b@gmail.com>
2014-08-04 19:34:20 +0200

    update-metablocks 1.2 and metablock formatting
    Signed-off-by: Joost Bremmer <toost.b@gmail.com>

--- batoto-direct-links/batoto_direct_links.user.js
+++ batoto-direct-links/batoto_direct_links.user.js
@@ -3,9 +3,11 @@
 // @namespace      https://github.com/ToostInc/userscripts
 // @description    Adds Direct Links to the online reader
 // @include        http://www.batoto.net/read/_/*
+// @author         Joost Bremmer < toost dot b at gmail dot com >
 // @copyright      2014, Joost Bremmer
 // @license        MIT
 // @version        1.0
+// @date           04-08-2014
 // @require        http://code.jquery.com/jquery-latest.min.js
 // @grant          GM_addStyle
 // @downloadURL    https://rawgit.com/ToostInc/userscripts/master/batoto-direct-links/batoto_direct_links.user.js
--- foolslide-download-links/foolslide-download-links.meta.js
+++ foolslide-download-links/foolslide-download-links.meta.js
@@ -3,9 +3,11 @@
 // @namespace    https://github.com/ToostInc/userscripts
 // @description  Adds Download Links to Foolslide reader links on a (front) page
 // @include      http://akashiscans.com/
+// @author         Joost Bremmer < toost dot b at gmail dot com >
 // @copyright    2014, Joost Bremmer
 // @license      MIT
 // @version      1.0
+// @date           04-08-2014  
 // @require      http://code.jquery.com/jquery-latest.min.js
 // @grant        none
 // @downloadURL  https://rawgit.com/ToostInc/userscripts/master/foolslide-download-links/foolslide-download-links.user.js
