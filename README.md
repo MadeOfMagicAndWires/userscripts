57e0a8c196dc38037b4bd8e7a04a6325fe8b02ba
Joost Bremmer <toost.b@gmail.com>
2014-08-04 07:38:21 +0200

    Youtube Hide Watched and licenses

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 userscripts
 ==================
-Userscripts I made over the years, and used to be up on userscripts.org  
+
+These are some userscripts I made over the years,
+some of which used to be up on userscripts.org  
+
 Most of them help with downloading manga/comic chapters from online readers,
 as I prefer offline readers like [MComix](http://sourceforge.net/projects/mcomix/).
 
@@ -11,10 +14,11 @@ Index
  * [Mangastream Torrent Links](#mangastream-torrent-links)
  * [MyAnimeList Hover Info](#myanimelist-hover-info)
  * [Patreon Creator Activity Only](#patreon-creator-activity-only)
+ * [Youtube Hide Watched](#youtube-hide-watched)
 
 <h2 id="botato-download-links">Botato Download links</h2>
 
-Adds direct links to every image of a current chapter on [Botato](http://http://www.batoto.net),
+Adds direct links to every image when reading a chapter on [Botato](http://http://www.batoto.net),
 so that they may be easily downloaded.
 
 
@@ -44,4 +48,6 @@ similar to the ones on Top Anime/Manga pages, to normal links.
 Hides non-creator posts on [Patreon](https://www.patreon.com/)
 from a projects Activity page.
 
+<h2 id="youtube-hide-watched">Youtube Hide Watched</h2>
 
+A no-nonsense userscript that hides watched videos from your subscription inbox on [Youtube](https://www.youtube.com/feed/subscriptions).
---
+++ youtube-hide-watched/youtube-hide-watched.user.js
@@ -0,0 +1,98 @@
+// ==UserScript==
+// @name           Youtube Hide Watched
+// @namespace      https://github.com/ToostInc/userscripts
+// @description    Hides viewed videos from your subscriptions.
+// @include        https://www.youtube.com/feed/subscriptions*
+// @copyright      2014, Joost Bremmer
+// @license        MIT
+// @version        1.0
+// @require        http://code.jquery.com/jquery-latest.min.js
+// @grant          unsafeWindow
+// ==/UserScript==
+
+// The MIT License
+//
+// Copyright (c) 2014 Joost Bremmer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and
+// to permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+$(document).ready (function () {
+	
+  if (MutationObserver) {
+	  var myObserver  = new MutationObserver(hideWatched);
+	}
+	else {
+		var myObserver = new WebKitMutationObserver(hideWatched);
+	}
+  myObserver.observe(document, { childList : true, subtree : true });
+	hideWatched();
+
+	// Add checkbox
+	var checker = '<li>\n'+
+	              '\t<label id="checker-container">\n'+
+				  '\t\t<input type="checkbox" id="hide-videos" checked="" />'+
+				  '\t\tHide watched videos'+
+				  '\t</label>\n'+
+				  '</li>';
+	$("#appbar-nav .appbar-nav-menu").prepend(checker);
+	$("#checker-container").css({
+	                              'color': "#666",
+								  "vertical-align" : "middle",
+								  "text-align" : "center"
+								});
+	
+	$("#hide-videos").change(function() {
+		if ( $(this).is(":not(:checked)") ) {
+			showWatched();
+		}
+	  
+		else {
+			hideWatched();
+		};
+		
+	});
+	
+	
+	//BONUS: always enable load more button.
+	$("button.load-more-button").removeProp("disabled");
+	
+	hideWatched();
+
+
+});
+
+
+function hideWatched () {
+	
+	if ( $("#hide-videos").is(":checked") ) {
+			$("div.watched-badge").each(function() {
+	      $(this).closest("li.feed-item-container").hide("400");
+		
+	    });
+
+	}
+};
+
+function showWatched() {
+		  $("div.watched-badge").each(function() {
+	      $(this).closest("li.feed-item-container").show("400");
+		
+	  });
+}
