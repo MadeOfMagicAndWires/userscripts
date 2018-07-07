e1f247d54391e6b24bfbd9aa03ec43a2067aca60
Joost Bremmer <contact@madeofmagicandwires.online>
2018-07-07 19:22:53 +0200

    whyfala: initial script

---
+++ whyfalala/whyfalalala.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           Whyfalalala download gallery
+// @namespace      https://github.com/ToostInc/userscripts
+// @description    Adds download links to scanlation posts
+// @include        https://whyfalalala.wordpress.com/*
+// @author         Joost Bremmer < contact@madeofmagicandwires.online >
+// @copyright      2018, Joost Bremmer
+// @license        MIT
+// @version       `0.1
+// @date           2108-07-07
+// @require        https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
+// ==/UserScript==
