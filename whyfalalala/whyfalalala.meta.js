8658ac5f4457362f2e31bfb76eaaa799ca97554a
Joost Bremmer <contact@madeofmagicandwires.online>
2018-07-11 19:46:05 +0200

    whyfalalala: Update regex to work with blogroll pages
    Update the @include rule to include /page/\d/ urls

--- whyfalalala/whyfalalala.meta.js
+++ whyfalalala/whyfalalala.meta.js
@@ -2,11 +2,11 @@
 // @name           Whyfalalala download WordPress gallery images
 // @namespace      https://github.com/ToostInc/userscripts
 // @description    Adds download links to posts containing a gallery
-// @include        /^https:\/\/whyfalalala\.wordpress\.com\/(\d{4}\/\d{2}\/\d{2}\/\S*\/(\#more-\d{4})?)?$/
+// @include        /^https:\/\/whyfalalala\.wordpress\.com\/(\d{4}\/\d{2}\/\d{2}\/\S*\/(\#more-\d{4})?|page\/\d{1,}\/)?$/
 // @author         Joost Bremmer < contact@madeofmagicandwires.online >
 // @copyright      2018, Joost Bremmer
 // @license        MIT
-// @version        1.0
+// @version        1.1
 // @date           2018-07-11
 // @require        https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
 // ==/UserScript==
