25c5402c981b8b6e4e9cd10e184794eb72eca534
Joost Bremmer <contact@madeofmagicandwires.online>
2019-09-23 04:22:00 +0200

    mangadex-toggle-tags: Add download and update links
    Signed-off-by: Joost Bremmer <contact@madeofmagicandwires.online>

--- mangadex-toggle-tags/mangadex-toggle-tags.meta.js
+++ mangadex-toggle-tags/mangadex-toggle-tags.meta.js
@@ -6,10 +6,10 @@
 // @author         Joost Bremmer < contact at made of magic and wires dot online >
 // @copyright      2019, Joost Bremmer
 // @license        MIT
-// @version        1.0.0
+// @version        1.0.1
 // @date           2019-09-23
-// @downloadURL
-// @updateURL
+// @downloadURL    https://github.com/MadeOfMagicAndWires/userscripts/raw/master/mangadex-toggle-tags/mangadex-toggle-tags.user.js
+// @updateURL      https://github.com/MadeOfMagicAndWires/userscripts/raw/master/mangadex-toggle-tags/mangadex-toggle-tags.user.js
 // @grant          none
 // @runat          document-end
 // ==/UserScript==
