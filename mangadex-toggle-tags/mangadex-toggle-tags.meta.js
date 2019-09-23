0718b5b28211d0caf3186c9bb326a590191e7d15
Joost Bremmer <contact@madeofmagicandwires.online>
2019-09-23 02:28:16 +0200

    Add mangadex-toggle-tags script
    Signed-off-by: Joost Bremmer <contact@madeofmagicandwires.online>

---
+++ mangadex-toggle-tags/mangadex-toggle-tags.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name           Mangadex Toggle Tags
+// @namespace      https://github.com/ToostInc/userscripts
+// @description    Hide or shows tags for spoiler purposes
+// @include        /^https?:\/\/mangadex\.org\/title\/.*$/
+// @author         Joost Bremmer < contact at made of magic and wires dot online >
+// @copyright      2019, Joost Bremmer
+// @license        MIT
+// @version        0.01
+// @date           2019-09-23
+// @downloadURL
+// @updateURL
+// @grant          none
+// @runat          document-end
+// ==/UserScript==
---
+++ mangadex-toggle-tags/mangadex-toggle-tags.user.js
@@ -0,0 +1,123 @@
+// ==UserScript==
+// @name           Mangadex Toggle Tags
+// @namespace      https://github.com/ToostInc/userscripts
+// @description    Hide or shows tags for spoiler purposes
+// @include        /^https?:\/\/mangadex\.org\/title\/.*$/
+// @author         Joost Bremmer < contact at made of magic and wires dot online >
+// @copyright      2019, Joost Bremmer
+// @license        MIT
+// @version        0.01
+// @date           2019-09-23
+// @downloadURL
+// @updateURL
+// @grant          none
+// @runat          document-end
+// ==/UserScript==
+
+/**
+  * Mangadex Toggle Tags
+  * Copyright © 2019 Joost Bremmer
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining
+  * a copy of this software and associated documentation files (the "Software"),
+  * to deal in the Software without restriction, including without limitation
+  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  * and/or sell copies of the Software, and to permit persons to whom the
+  * Software is furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included
+  * in all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  */
+
+"use strict";
+
+
+/**
+ * Toggles the visibility state of tags
+ *
+ * @param {HTMLElement} container contains tag elements to show or hide
+ * @param {Boolean} visibilityOverride overrides usual checks to force
+ *                                     visibilty state
+ * @return {undefined}
+ **/
+function toggleTags(container, visibilityOverride) {
+  console.log(container);
+
+  let toggleBtn = document.getElementById("toggleTags");
+
+  if(toggleBtn) {
+    let tagsVisible = visibilityOverride ? visibilityOverride : ("tagsVisible" in toggleBtn.dataset);
+
+    console.log(tagsVisible);
+
+    // loop
+    Array.prototype.forEach.call(container.getElementsByTagName("a"), anchor => {
+      if(anchor !== toggleBtn) {
+        console.log(tagsVisible);
+        anchor.style.display = tagsVisible ? "inline-block" : "none";
+      } else {
+        anchor.innerText = tagsVisible ? "Hide Tags" : "Show Tags";
+      }
+    });
+
+    // toggle saved state
+    if("tagsVisible" in toggleBtn.dataset) {
+      delete toggleBtn.dataset.tagsVisible;
+    } else {
+      toggleBtn.dataset.tagsVisible = "";
+    }
+  }
+}
+
+/**
+ * Inserts a button that can be clicked to show or hide the tags
+ * @param {HTMLElement} container parent element to insert button into
+ * @return {undefined}
+ *
+ **/
+function addToggleButton(container) {
+
+  let toggleBtn = document.createElement("a");
+
+  // set toggle button details
+  toggleBtn.id = "toggleTags";
+  toggleBtn.classList.add("badge");
+  toggleBtn.style.color = "#4c5760";
+  toggleBtn.style.background = "#fff";
+  toggleBtn.innerText = "Show Tags";
+  toggleBtn.addEventListener("click", () => {
+    console.log(("hideTags" in toggleBtn.dataset));
+
+    // set visibility according to saved state
+    toggleTags(container);
+
+  });
+
+
+  // insert toggle button
+  container.innerHTML = "&nbsp;" + container.innerHTML;
+  container.insertBefore(toggleBtn, container.childNodes[0]);
+}
+
+/**
+ * Main function that runs the script; to be called once DOM is loaded
+ * @return {undefined}
+ **/
+function main() {
+  let container = document.querySelector("div.m-0:nth-child(5) > div:nth-child(2)");
+
+  addToggleButton(container);
+  toggleTags(container, false);
+}
+
+document.addEventListener("DOMContentLoaded", main);
+
+// vim: set ts=2 sts=2 sw=2 et :
